f8e4fda065bde464e626cc68f76c1643c31c976e
EugeniaCeinos <eceinoss@uwyo.edu>
2024-09-24 00:41:38 +0000

    Done

--- cosc1010-lab03.py
+++ cosc1010-lab03.py
@@ -4,9 +4,6 @@
 # Lab 03 
 # Lab Section: 16
 # Sources, people worked with, help given to: https://www.w3schools.com/python/ref_list_remove.asp I used that page to check some functions
-# your
-# comments
-# here
 
 
 
@@ -31,13 +28,15 @@ print(states[0])
 print(states[-1])
 
 #Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
-print(f"{states[1].upper() is south of {states[0].upper()}
+print(f"{states[1].upper()} is south of {states[0].upper()}")
 
 
 
 print("Part Two------------------------------------------------------------------------")
 #Append the following states to your list: Washington, Oregon, California and print your list
-states.append("Washington", "Oregon", "California")
+states.append("Washington")
+states.append("Oregon")
+states.append("California")
 print(states)
 
 #Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
@@ -57,11 +56,11 @@ states.remove("Texas")
 
 print("Part Three----------------------------------------------------------------------")
 #Temporarily sort your list, print it both sorted and unsorted 
-print(states.sorted())
+print(sorted(states))
 print(states)
 
 #Permanently sort your list in reverse order, printing it out
-states.sort()
+states.sort(reverse=True)
 print(states)
 
 #Using the reverse method reverse the list and print it
